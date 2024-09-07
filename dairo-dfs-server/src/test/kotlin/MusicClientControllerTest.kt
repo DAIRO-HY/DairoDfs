bc13da1575049eb1463183cb330e3c222829f998
zhoulq <615741631@qq.com>
2024-09-07 21:43:30 +0900

    回收站数据定期删除完成

--- dairo-dfs-server/src/test/kotlin/MusicClientControllerTest.kt
+++ dairo-dfs-server/src/test/kotlin/MusicClientControllerTest.kt
@@ -1,10 +1,5 @@
 import cn.dairo.dfs.DairoApplication
-import cn.dairo.dfs.boot.BootTimer
-import cn.dairo.dfs.config.SystemConfig
-import cn.dairo.dfs.dao.DfsFileDao
-import cn.dairo.dfs.dao.dto.DfsFileDto
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest(classes = [DairoApplication::class])
